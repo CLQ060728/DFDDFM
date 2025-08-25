32f25d418e9227ccf8da561d3d21672389c1c85b
Qian Liu <liu.qian.pro@gmail.com>
2025-08-25 10:14:05 +0200

    change to unix EOF and change to dropbox link for openai clip install.sh

--- install.sh
+++ install.sh
@@ -3,10 +3,13 @@ mkdir -p pre_trained/META_DINOV2/
 mkdir -p pre_trained/META_DINOV3/LVD/
 mkdir -p pre_trained/META_DINOV3/SAT/
 
+sudo apt install unzip
 pip uninstall torchaudio
 pip install --upgrade -r requirements.txt
 
-gdown "https://drive.google.com/drive/folders/1fm3Jd8lFMiSP1qgdmsxfqlJZGpr_bXsx?usp=drive_link" -O pre_trained/OPENAI_CLIP/ --folder
+# gdown "https://drive.google.com/drive/folders/1fm3Jd8lFMiSP1qgdmsxfqlJZGpr_bXsx?usp=drive_link" -O pre_trained/OPENAI_CLIP/ --folder
+wget "https://www.dropbox.com/scl/fi/9e1fdt4u3g9npeixsg61d/OPENAI_ViT_L_14.zip?rlkey=vz5fn421z0mvh4rvapnim30fg&st=drtu25ka&dl=0" -O pre_trained/OPENAI_ViT_L_14.zip
+unzip pre_trained/OPENAI_ViT_L_14.zip -d pre_trained/OPENAI_CLIP/
 hf download "facebook/dinov2-large" --local-dir pre_trained/META_DINOV2/ --cache-dir pre_trained/META_DINOV2/temp/
 hf download "facebook/dinov3-vitl16-pretrain-lvd1689m" --local-dir pre_trained/META_DINOV3/LVD/ --cache-dir pre_trained/META_DINOV3/LVD/temp/
 hf download "facebook/dinov3-vitl16-pretrain-sat493m" --local-dir pre_trained/META_DINOV3/SAT/ --cache-dir pre_trained/META_DINOV3/SAT/temp/
