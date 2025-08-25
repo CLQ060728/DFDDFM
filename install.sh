ddf9f3ed1e8783f2b4837b73e9b9531475a1a3df
CLQ060728 <liu.qian.pro@gmail.com>
2025-08-25 08:25:41 +0000

    clean up after openai_clip download added

--- install.sh
+++ install.sh
@@ -10,6 +10,10 @@ pip install --upgrade -r requirements.txt
 # gdown "https://drive.google.com/drive/folders/1fm3Jd8lFMiSP1qgdmsxfqlJZGpr_bXsx?usp=drive_link" -O pre_trained/OPENAI_CLIP/ --folder
 wget "https://www.dropbox.com/scl/fi/9e1fdt4u3g9npeixsg61d/OPENAI_ViT_L_14.zip?rlkey=vz5fn421z0mvh4rvapnim30fg&st=drtu25ka&dl=0" -O pre_trained/OPENAI_ViT_L_14.zip
 unzip pre_trained/OPENAI_ViT_L_14.zip -d pre_trained/OPENAI_CLIP/
+mv pre_trained/OPENAI_CLIP/OPENAI_ViT_L_14/** pre_trained/OPENAI_CLIP/
+rm -rf pre_trained/OPENAI_ViT_L_14.zip
+rm -rf pre_trained/OPENAI_CLIP/OPENAI_ViT_L_14/
+
 hf download "facebook/dinov2-large" --local-dir pre_trained/META_DINOV2/ --cache-dir pre_trained/META_DINOV2/temp/
 hf download "facebook/dinov3-vitl16-pretrain-lvd1689m" --local-dir pre_trained/META_DINOV3/LVD/ --cache-dir pre_trained/META_DINOV3/LVD/temp/
 hf download "facebook/dinov3-vitl16-pretrain-sat493m" --local-dir pre_trained/META_DINOV3/SAT/ --cache-dir pre_trained/META_DINOV3/SAT/temp/
