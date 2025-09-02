7a441f4d3d571d4bc0f5162692d1fa594ccc52af
CLQ060728 <liu.qian.pro@gmail.com>
2025-09-02 06:24:17 +0000

    stop tracking configs/default_config.yaml after installation

--- install.sh
+++ install.sh
@@ -23,3 +23,4 @@ rm -rf pre_trained/META_DINOV2/temp/
 rm -rf pre_trained/META_DINOV3/LVD/temp/
 rm -rf pre_trained/META_DINOV3/SAT/temp/
 pip cache purge
+git update-index --skip-worktree configs/default_config.yaml
